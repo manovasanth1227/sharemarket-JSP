7fbb199938a87d7a493f46d77c6edfeed26cedcf
manovasanth1227 <57672259+manovasanth1227@users.noreply.github.com>
2021-05-05 19:54:46 +0530

    Delete ShareMarketJSP directory

--- ShareMarketJSP/src/Model/Order.java
+++
@@ -1,8 +0,0 @@
-package Model;
-
-
-public interface Order {
-	public boolean marketOrder(int id , int stockid , String symbol ,  String investmentType ,String orderType, String orderOption , int qty);
-	public boolean limitOrder(int id , int stockid ,String symbol ,  String investmentType ,String orderType, String orderOption , int qty , double price);
-	public void bracketOrder(int id , int stockid ,String symbol ,  String investmentType ,String orderType, String orderOption , int qty , double price, double stoploss);
-}
